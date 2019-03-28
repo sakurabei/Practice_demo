72b473fb21c119a0d941ebed0f0ea98079a4cf9a
sakurabei <bei@bllli.cn>
2019-03-28 23:02:36 +0800

    移动端课程列表页——banner图实现

--- 移动端课程列表页/js/index.js
+++ 移动端课程列表页/js/index.js
@@ -24,20 +24,44 @@
     recalc();
 })(document, window);
 
-var innerGroup = $(".innerwraper");
+var innerGroup = $(".innerwrapper");
 var leftArrow = $(".left-arrow");
 var rightArrow = $(".right-arrow");
 var spanGroup = $(".pagination span");
-var imgWidth = $(".innerwraper img:first-child").eq(0).width();
+var imgWidth = $(".innerwrapper img:first-child").eq(0).width();
 var _index = 0;
 var timer = null;
+// 点击切换
 spanGroup.on("click", function() {
     //导航切换
     _index = spanGroup.index($(this));
     selectPic(_index);
 
 })
-
+function selectPic(num) {
+    // 重置操作
+    clearInterval(timer);
+    //让我们点击的小圆点背景色变白，其余小圆点，没有背景色
+    $(".pagination span").eq(num).addClass("active").siblings().removeClass("active");
+    //自动播放，小圆点切换至最后的时候,再次切换的时候呢,让第一个小圆点变白,周而复始
+    if (num % 4 == 0) {
+        $(".pagination span").eq(0).addClass("active").siblings().removeClass("active");
+    }
+    $(".inner").stop().animate({
+        left: -num * imgWidth,
+    }, 1000, function() {
+        //点击切换图片效果结束以后3秒,要开始自动播放啦
+        timer = setInterval(go, 3000);
+        //自动播放检查是否到最后一张
+        if (_index == innerGroup.length - 1) {
+            //最后一张图片的时候,让_index = 0
+            _index %= 4;
+            // 从最后一张直接瞬间换成第一张图片
+            $(".inner").css("left", "0px");
+        }
+    })
+}
+// 自动切换
 function autoGo() {
     //自动行走
     timer = setInterval(go, 3000);
@@ -51,24 +75,3 @@ function go() {
     selectPic(_index);
 }
 
-function selectPic(num) {
-    clearInterval(timer);
-    //让我们点击的小圆点背景色变白
-    $(".pagination span").eq(num).addClass("active").siblings().removeClass("active");
-    //小圆点切换至最后的时候,再次切换的时候呢,让第一个小圆点变白,周而复始
-    if (num % 4 == 0) {
-        $(".pagination span").eq(0).addClass("active").siblings().removeClass("active");
-    }
-    $(".inner").stop().animate({
-        left: -num * imgWidth,
-    }, 1000, function() {
-        //点击切换图片效果结束以后,要开始自动播放啦
-        timer = setInterval(go, 3000);
-        //自动播放检查是否到最后一张
-        if (_index == innerGroup.length - 1) {
-            //最后一张图片的时候,让_index = 0
-            _index %= 4;
-            $(".inner").css("left", "0px");
-        }
-    })
-}
